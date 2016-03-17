6633692252a558693e51f19cc0ebce661fe21af7
mfrancis95 <mikefrancis95@gmail.com>
2016-03-17 11:04:30 -0400

    Add non-class functions

--- highlighter.py
+++ highlighter.py
@@ -1,16 +1,27 @@
+from os.path import splitext
 from re import sub
+
+defaultColors = ["\033[31m", "\033[32m", "\033[33m"]
 
 class Highlighter:
 
     def __init__(self):
-        self.colors = ["\033[31m", "\033[32m", "\033[33m"]
-        self.patterns = {}
+        self.colors = defaultColors
+        self.styles = {}
 
-    def colorize(self, match, color):
-        return self.colors[color] + sub("\033\[..m", "", match.group()) + "\033[39m"
+    def highlight(self, file):
+        styles = self.styles.get(splitext(file)[1][1:], [])
+        with open(file) as f:
+            for line in highlight(f, styles, self.colors):
+                yield line
 
-    def highlight(self, file, fileType):
-        for line in file:
-            for pattern in self.patterns[fileType]:
-                line = sub(pattern["pattern"], lambda match: self.colorize(match, pattern["color"]), line)
-            yield line
+def colorize(text, color):
+    return color + sub("\033\[..m", "", text) + "\033[39m"
+
+def highlight(file, styles, colors = None):
+    if not colors:
+        colors = defaultColors
+    for line in file:
+        for style in styles:
+            line = sub(style["pattern"], lambda match: colorize(match.group(), colors[style["color"]]), line)
+        yield line
